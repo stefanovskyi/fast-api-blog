4073e1e366d3769d57d204cf9cc6366bd68a8e2e
Oleksandr Stefanovskyi <ostefanovskyi@intelliarts.com>
2021-07-09 14:24:24 +0300

    init blog

---
+++ blog/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return blog
